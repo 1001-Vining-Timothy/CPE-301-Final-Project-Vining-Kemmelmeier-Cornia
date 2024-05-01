67c09347ebe673d3397f974e0f292902a520291d
Lena Kemmelmeier <97976019+Lena-Kemmelmeier@users.noreply.github.com>
2024-05-01 11:12:04 -0700

    Add files via upload

---
+++ fan_motor_start_in_progress.ino
@@ -0,0 +1,17 @@
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  //pinMode(5, OUTPUT); 
+}
+
+void loop()
+{
+ 
+  
+ 
+  digitalWrite(4, LOW);
+  digitalWrite(3, HIGH);
+  //digitalWrite(5,HIGH);
+  //analogWrite(5,255);
+}
